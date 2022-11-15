7e57ab017a7f550c188f9845c0ffdaa039362d55
iceberg <iceberg.hbs@qq.com>
2022-11-15 22:17:45 +0800

    I become a Java learner today

---
+++ FirstClass.java
@@ -0,0 +1,8 @@
+public class FirstClass{
+    public static void main(String[] args){
+        System.out.println("This is gonna to be my very first Java class!");
+        System.out.println("The file name must be the same as class nmae in Java.");
+        System.out.println("What's more, one file one class.");
+        // this is my first comment in Java.
+    }
+}
